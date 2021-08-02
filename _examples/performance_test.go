e4a65867c9fce5d6d4bfa4a9939d1a967f38f377
FishGoddess <1149062639@qq.com>
2021-08-03 00:35:00 +0800

    增加连接池

--- _examples/performance_test.go
+++ _examples/performance_test.go
@@ -9,17 +9,21 @@
 package main
 
 import (
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/FishGoddess/vex"
 )
 
-// BenchmarkServer-16        110065             14936 ns/op             144 B/op         11 allocs/op
+// BenchmarkServer-16        112126             12759 ns/op             144 B/op         11 allocs/op
 
 const (
 	// dataSize is the data size of test.
 	dataSize = 10000
+
+	// concurrency is the concurrency of test.
+	concurrency = 10000
 
 	// benchmarkCommand is the command of benchmark.
 	benchmarkCommand = byte(1)
@@ -34,8 +38,24 @@ func testTask(task func(no int)) string {
 	return time.Now().Sub(beginTime).String()
 }
 
-// go test -v -run=^TestVexServer$
-func TestVexServer(t *testing.T) {
+// testTaskConcurrent is a wrapper wraps task to testTask with concurrency.
+func testTaskConcurrent(task func(no int)) string {
+
+	wg := &sync.WaitGroup{}
+	beginTime := time.Now()
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func(no int) {
+			defer wg.Done()
+			task(no)
+		}(i)
+	}
+	wg.Wait()
+	return time.Now().Sub(beginTime).String()
+}
+
+// go test ./_examples/performance_test.go -v -run=^TestServerRPS$
+func TestServerRPS(t *testing.T) {
 
 	resp := []byte("test")
 	param1 := []byte("one")
@@ -70,7 +90,46 @@ func TestVexServer(t *testing.T) {
 	t.Logf("Taken time is %s!\n", takenTime)
 }
 
-// go test -v -run=^$ -bench=^BenchmarkServer$ -benchtime=1s
+// go test ./_examples/performance_test.go -v -run=^TestServerRPSWithPool$
+func TestServerRPSWithPool(t *testing.T) {
+
+	resp := []byte("test")
+	param1 := []byte("one")
+	param2 := []byte("two")
+
+	server := vex.NewServer()
+	server.RegisterHandler(benchmarkCommand, func(args [][]byte) (body []byte, err error) {
+		return resp, nil
+	})
+	defer server.Close()
+
+	go func() {
+		err := server.ListenAndServe("tcp", ":5837")
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	pool, err := vex.NewClientPool("tcp", "127.0.0.1:5837", 64)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer pool.Close()
+
+	takenTime := testTaskConcurrent(func(no int) {
+		client := pool.Get()
+		defer pool.Put(client)
+
+		body, err := client.Do(benchmarkCommand, [][]byte{param1, param2})
+		if err != nil {
+			t.Fatal(err, body)
+		}
+	})
+
+	t.Logf("Taken time is %s!\n", takenTime)
+}
+
+// go test ./_examples/performance_test.go -v -run=^$ -bench=^BenchmarkServer$ -benchtime=1s
 func BenchmarkServer(b *testing.B) {
 
 	resp := []byte("test")
---
+++ pool_test.go
@@ -0,0 +1,58 @@
+// Copyright 2021 Ye Zi Jie.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+//
+// Author: FishGoddess
+// Email: fishgoddess@qq.com
+// Created at 2021/08/02 23:44:46
+
+package vex
+
+import (
+	"runtime"
+	"testing"
+	"time"
+)
+
+// go test -v -cover -run=^TestNewClientPool$
+func TestNewClientPool(t *testing.T) {
+
+	server := NewServer()
+	server.RegisterHandler(1, func(args [][]byte) (body []byte, err error) {
+		return []byte("test"), nil
+	})
+	defer server.Close()
+
+	go func() {
+		err := server.ListenAndServe("tcp", ":5837")
+		if err != nil {
+			panic(err)
+		}
+	}()
+	runtime.Gosched()
+	time.Sleep(10 * time.Millisecond)
+
+	pool, err := NewClientPool("tcp", "127.0.0.1:5837", 64)
+	if err != nil {
+		t.Fatal("new client pool failed", err)
+	}
+	defer pool.Close()
+
+	for i := 0; i < 512; i++ {
+
+		client := pool.Get()
+		response, err := client.Do(1, [][]byte{
+			[]byte("123"), []byte("456"),
+		})
+		if err != nil {
+			pool.Put(client)
+			t.Fatalf("do command %d failed with %+v", i, err)
+		}
+
+		if string(response) != "test" {
+			pool.Put(client)
+			t.Fatalf("response %s is wrong", string(response))
+		}
+		pool.Put(client)
+	}
+}

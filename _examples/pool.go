e6c53d32b3ba0bd74de11559dc4a33abd73c6da0
FishGoddess <1149062639@qq.com>
2023-07-06 18:34:47 +0800

    基本重构完成

---
+++ _examples/pool.go
@@ -0,0 +1,40 @@
+// Copyright 2023 FishGoddess. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+
+	"github.com/FishGoddess/vex"
+	"github.com/FishGoddess/vex/pool"
+)
+
+func main() {
+	// We provide a client pool for limiting the number of clients.
+	// It needs a dial function to create a new client when it needs.
+	// You can pass any client options to Dial to create the client as usual.
+	clientPool := pool.New(pool.Dial("127.0.0.1:6789"))
+	defer clientPool.Close()
+
+	client, err := clientPool.Get(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Close()
+
+	// Use client as usual.
+	// Also, you can customize your dial function:
+	dial := func() (vex.Client, error) {
+		// You can do anything you want to customize the client.
+		return vex.NewClient("127.0.0.1:6789", vex.WithReadBufferSize(4096), vex.WithWriteBufferSize(4096))
+	}
+
+	pool.New(dial)
+
+	// The pool has some default configurations like max connections.
+	// If you want to change it, check pool.Option in New parameters.
+	pool.New(dial, pool.WithConnections(16), pool.WithMaxIdle(8))
+}
